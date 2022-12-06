98f31090380e995d31ec76eaa3d6cb210a63f010
Farshid Mousavi <44027250+farshidmousavi@users.noreply.github.com>
2022-12-06 15:17:41 +0300

    Update and rename 5-Type check(junior).py to 5-Type check.py

--- 5-Type check.py
+++ 5-Type check.py
@@ -3,11 +3,7 @@
 def only_ints(a,b):
     try:
         if type(a)==int and type(b)==int:
-            if isinstance(a,int) and isinstance(b,int):
-                return True
-            
+            return True
         return False
     except:
         return False
-        
-print(only_ints(1,True))
