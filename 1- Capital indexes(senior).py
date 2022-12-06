e5ec87b098629b511794dd6729674777e431c71d
Farshid Mousavi <44027250+farshidmousavi@users.noreply.github.com>
2022-12-06 14:58:25 +0300

    Create 1- Capital indexes(senior).py

---
+++ 1- Capital indexes(senior).py
@@ -0,0 +1,5 @@
+#This was the secound way i resolve the problem (or answer the question)
+#How did you do?
+#Insert comment for me
+def capital_indexes(word):
+    return [i for i in range(len(word)) if word[i].isupper() ]
